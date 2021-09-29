b7ae53816871c575e1385616401488227b4a0121
Triloworld <2671726+Triloworld@users.noreply.github.com>
2021-09-29 14:57:59 +0200

    Update swiper

--- configuration/css.js
+++ configuration/css.js
@@ -1,5 +1,5 @@
 export default [
-  'swiper/swiper.min.css',
+  'swiper/swiper-bundle.min.css',
   '~/assets/css/gdpr.css',
   '~/assets/css/animate-border.css',
   '~/assets/css/border-less-height.css',
