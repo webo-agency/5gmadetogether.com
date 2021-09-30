ea212d1700e8d60c6083d9cee1530899f68f8c81
Triloworld <2671726+Triloworld@users.noreply.github.com>
2021-09-30 11:21:23 +0200

    Swiper fixes

--- configuration/css.js
+++ configuration/css.js
@@ -1,5 +1,4 @@
 export default [
-  '~/assets/css/swiper.min.css',
   '~/assets/css/gdpr.css',
   '~/assets/css/animate-border.css',
   '~/assets/css/border-less-height.css',
--- plugins/vue-awesome-swiper.js
+++ plugins/vue-awesome-swiper.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
-import VueAwesomeSwiper from 'vue-awesome-swiper';
+import VueAwesomeSwiper from "vue-awesome-swiper/dist/exporter";
 
-// import custom style
-// import '~/assets/css/swiper.css';
+import { Swiper as SwiperClass, Pagination, Navigation, Autoplay } from "swiper/swiper.esm";
 
-Vue.use(VueAwesomeSwiper);
+// import style
+import "swiper/swiper-bundle.min.css"
+
+SwiperClass.use([Pagination, Navigation, Autoplay])
+Vue.use(VueAwesomeSwiper(SwiperClass))
+
+const { directive } = VueAwesomeSwiper(SwiperClass)
+Vue.directive("swiper", directive)
