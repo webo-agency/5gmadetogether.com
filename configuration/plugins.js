9d4c569fa64e7b45a4f6c7b893d93cb98cee0fde
revinto <jakub.gornikk@gmail.com>
2022-08-08 20:23:01 +0200

    CompositionAPI dependencies

--- configuration/plugins.js
+++ configuration/plugins.js
@@ -1,4 +1,5 @@
 export default [
+  { src: "~/plugins/vue-composition-api.js", mode: "server" },
   { src: '~/plugins/webfontloader.js', mode: 'client' },
   { src: '~/plugins/vue-lazytube.js', mode: 'client' },
   { src: '~/plugins/vue-count-to.js' },
