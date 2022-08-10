73949e5e7c84c47907bd201311bf7c2ba58b18ef
revinto <jakub.gornikk@gmail.com>
2022-08-10 09:22:44 +0200

    AboutConferences fixes

--- components/AboutConference.vue
+++ components/AboutConference.vue
@@ -17,7 +17,7 @@
                 <img src="../static/img/Conference.png" alt="">
             </div>
         </div>
-        <p class="w-ful text-basel leading-relaxed mt-[4rem] mx-auto max-w-4xl">Among the leading experts from the world of science and business, we want to talk about the future of telecommunications and what awaits us after 5G. Work on the next generations of telecommunications standards is underway in many places around the world – with this event we want to give the opportunity to meet and talk to those who develop technologies and those who will implement and commercialize them.</p>
+        <p class="w-full text-basel leading-relaxed mt-[4rem] mx-auto max-w-4xl">Among the leading experts from the world of science and business, we want to talk about the future of telecommunications and what awaits us after 5G. Work on the next generations of telecommunications standards is underway in many places around the world – with this event we want to give the opportunity to meet and talk to those who develop technologies and those who will implement and commercialize them.</p>
     </section>
 </template>
 <script>
