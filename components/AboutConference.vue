67c174fa4a215f154a1ca23ad541933b9d300a61
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-09-30 15:27:17 +0200

    Fix bad behaviour

--- components/AboutConference.vue
+++ components/AboutConference.vue
@@ -1,30 +1,50 @@
 <template>
-    <section id="about" class="mb-36">
-            <div class="relative pt-12 container">
-            <div class="relative flex flex-col justify-start z-10">
-                <div class="flex items-center space-x-2 ml-[4.5rem]">
-                    <svg width="43" height="40" viewBox="0 0 43 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0 40L31 -1.35505e-06L43 -1.87959e-06L12 40L0 40Z" fill="#39E8EA"/>
-                    </svg>
-                    <h5 class="text-[12px] font-medium leading-snug mt-3">ABOUT THE <span class="text-[#39E8EA]">CONFERENCE - </span> </h5>
-                </div>
-                <h3 class="ml-[7.5rem] text-[35px] font-medium max-w-[33rem] leading-snug">Discover challanges and visions of 5G wireless network</h3>
-          </div>
-            <div class="container mt-[4rem]">
-                <div class="ml-28">
-                    <p class="leading-relaxed text-[#002738] max-w-2xl">Among the leading experts from the world of science and business, we want to talk about the future of telecommunications and what awaits us after 5G. Work on the next generations of telecommunications standards is underway in many places around the world – with this event we want to give the opportunity to meet and talk to those who develop technologies and those who will implement and commercialize them.</p>
-                </div>
-            </div>
-            <div class="absolute top-0 left-16 z-0">
-                <img src="../static/img/About.png" alt="">
-            </div>
-            <div class="absolute top-[6.5rem] left-40 z-0">
-                <img src="../static/img/Conference.png" alt="">
-            </div>
-
+  <section id="about" class="mb-36">
+    <div class="relative pt-12 container">
+      <div class="relative flex flex-col justify-start z-10">
+        <div class="flex items-center space-x-2 ml-[4.5rem]">
+          <svg
+            width="43"
+            height="40"
+            viewBox="0 0 43 40"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0 40L31 -1.35505e-06L43 -1.87959e-06L12 40L0 40Z"
+              fill="#39E8EA"
+            />
+          </svg>
+          <h5 class="text-[12px] font-medium leading-snug mt-3">
+            ABOUT THE <span class="text-[#39E8EA]">CONFERENCE - </span>
+          </h5>
         </div>
-    </section>
+        <h3
+          class="ml-[7.5rem] text-[35px] font-medium max-w-[33rem] leading-snug"
+        >
+          Discover challanges and visions of 5G wireless network
+        </h3>
+      </div>
+      <div class="container mt-[4rem]">
+        <div class="ml-28">
+          <p class="leading-relaxed text-[#002738] max-w-2xl">
+            Among the leading experts from the world of science and business, we
+            want to talk about the future of telecommunications and what awaits
+            us after 5G. Work on the next generations of telecommunications
+            standards is underway in many places around the world – with this
+            event we want to give the opportunity to meet and talk to those who
+            develop technologies and those who will implement and commercialize
+            them.
+          </p>
+        </div>
+      </div>
+      <div class="absolute top-0 left-16 z-0">
+        <img src="/static/img/About.png" alt="" />
+      </div>
+      <div class="absolute top-[6.5rem] left-40 z-0">
+        <img src="/static/img/Conference.png" alt="" />
+      </div>
+    </div>
+  </section>
 </template>
-<script>
-
-</script>
+<script></script>
