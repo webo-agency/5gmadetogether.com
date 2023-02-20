c1a38ea1bd7f78d7b641875351cf9c10e52bf236
Tomek <TK-54-4B>
2023-02-20 20:04:35 +0100

    test

--- configuration/head.js
+++ configuration/head.js
@@ -2,10 +2,10 @@ export default {
   title: "5gmadetogether.com",
   meta: [
     { charset: "utf-8" },
-    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" }
   ],
   bodyAttrs: {
-    class: 'home-page home page '
+    class: "home-page home page "
   },
   script: [
     // { src: `https://polyfill.io/v3/polyfill.min.js?features=${features}&flags=gated`, body: true, async: true, defer: true  },
@@ -14,7 +14,7 @@ export default {
     {
       rel: "preconnect",
       href: "https://fonts.gstatic.com",
-      crossorigin: true
+      crossorigin: false
     }
   ]
-}
+};
